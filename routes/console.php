07f2460c4fc21ec02aa22b645cda96523bfc494c
Lucas Ayala Casanova <71414356+HatCliff@users.noreply.github.com>
2025-03-20 15:42:42 -0300

    Pruebas con laravel
    Laravel test

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('negrea_al_dan',function(){
+    $this->comment('¿Que hacemos? Tlabaja, tiene que tlabajal. Mañana ocho mañana. No,no,no. Levántate. Mañana tienes que trabajar.Plata tú no plata');
+})->purpose('TLABAJA');
+
+
+Artisan::command('negrea_al_pipe',function(){
+    $this->comment('¿Que hacemos? Tlabaja, tiene que tlabajal. Mañana ocho mañana. No,no,no. Levántate. Mañana tienes que trabajar.Plata tú no plata');
+})->purpose('TLABAJA');
+
+
+Artisan::command('negrea_a_sele',function(){
+    $this->comment('¿Que hacemos? Tlabaja, tiene que tlabajal. Mañana ocho mañana. No,no,no. Levántate. Mañana tienes que trabajar.Plata tú no plata');
+})->purpose('TLABAJA');
+
+
+Artisan::command('negrea_al_lucas',function(){
+    $this->comment('Deja que los otros hagan todo, tu descansa mi rei 😎👌');
+})->purpose('TLABAJA');
