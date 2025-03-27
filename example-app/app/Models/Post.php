16bf375697d799185b9044f3fd07b0eb7db6a4dd
feliger27 <feligerbier@gmail.com>
2025-03-27 15:31:49 -0300

    example-app

---
+++ example-app/app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'body',
+      ];
+}
--- resources/views/tlabaja.blade.php
+++
@@ -1,3 +0,0 @@
-<h3>
-¿Que hacemos? Tlabaja, tiene que tlabajal. Mañana ocho mañana. No,no,no. Levántate. Mañana tienes que trabajar.Plata tú no plata
-</h3>
