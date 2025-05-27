1ee36b8973930b6570b768912b23a1f3c1aba7fb
feliger27 <fgerbier@ing.ucsc.cl>
2025-05-26 22:45:48 -0400

    model y migration insumos

---
+++ database/migrations/2025_05_27_022700_create_insumos_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('insumos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('tipo');
+            $table->string('unidad_medida');
+            $table->integer('stock')->default(0);
+            $table->text('descripcion')->nullable();
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('insumos');
+    }
+};
